844b053bdc59876de545da604b714249d5f993b6
Xyzzyall <xyzzyall@gmail.com>
2017-11-18 22:54:54 +0300

    начало работы над редактором

--- Editor/src/Editor/GameThread.java
+++ Editor/src/Editor/GameThread.java
@@ -1,11 +1,13 @@
 package Editor;
 
 import Main.Game;
+import Wraps.Wrap;
 
 public class GameThread extends Thread
 {
     public Game game;
-
+    public static Wrap currentWrap = null;
+    public static byte brushMode = 0; //0 -- редачим то, что видим, 1 -- создать бэкграунд, 2 -- ставить спрайты, 3 -- ставить декали
     public Editor editor;
 
     public GameThread(int width, int height, int fps)
@@ -18,7 +20,7 @@ public class GameThread extends Thread
         game.map = editor;
     }
 
-    
+
 
     @Override
     public void run()
